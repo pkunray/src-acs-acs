f81b67d4089879258878e5917cde872f235d5a8d
pkunray <pkunray@gmail.com>
2024-01-09 19:50:14 +0100

    practice: express, rest client

--- node-example/index.js
+++ node-example/index.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+// for parsing application/json 
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/greet/:name', (req, res) => {
+  res.send(`<h2>Hello ${req.params.name}!</h2>`)
+})
+
+app.get('/translate/:lang/:text', (req, res) => {
+  // call google transtation api
+})
+
+app.post('/post', (req, res) => {
+  const body = req.body
+  res.send('Hello ' + body.name);
 })
 
 app.listen(port, () => {
